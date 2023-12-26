7061cc5338fec5dc1dcc630d3e48a8f1276a9ba1
Beyzanur Eryılmaz <eryilmazbeyza@gmail.com>
2023-12-26 18:41:13 +0300

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 
 import "./App.css";
-import Header from "./components/Header";
+import Footer from "./components/Layout/Footer/Footer";
+import Header from "./components/Layout/Header/Header";
+import Policy from "./components/Layout/Policy/Policy";
+import Sliders from "./components/Slider/Sliders";
 
 function App() {
   return (
     <div>
       <Header/>
-      <h1>Hello</h1>
+      <Sliders/>
+      <Policy/>
+      <Footer/>
     </div>
   );
 }
