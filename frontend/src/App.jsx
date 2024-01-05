cdcacdc14be3bf6780ec2c3197a0c296baf363a1
Beyzanur Eryılmaz <eryilmazbeyza@gmail.com>
2024-01-05 23:05:26 +0300

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
 
-import "./App.css";
+import Categories from "./components/Categories/Categories"
 import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
 import Policy from "./components/Layout/Policy/Policy";
+import Products from "./components/Products/Products";
+import Campaigns from "./components/Campaigns/Campaigns";
+import Blogs from "./components/Blogs/Blogs";
 import Sliders from "./components/Slider/Sliders";
+import Brands from "./components/Brands/Brands";
+import CampaignSingle from "./components/CampaignSingle/CampaignSingle";
+import "./App.css";
+
 
 function App() {
   return (
     <div>
       <Header/>
       <Sliders/>
+      <Categories/>
+      <Products/>
+      <Campaigns/>
+      <Products/>
+      <Blogs/>
+      <Brands/>
+      <CampaignSingle/>
       <Policy/>
       <Footer/>
     </div>
